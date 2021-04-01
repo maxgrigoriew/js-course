67edaa148066d26f7b7b86e4cf2e10c6763781b0
Max <sss123123111@yandex.ru>
2021-04-01 19:33:25 +0300

    first lesson

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-let name = 1;
+let money = 1;
+let income = 'string';
+let addExpenses = true;
+let deposit = 'number';
+let mission = 23;
+let period = 100;
+alert('привет мир');
+console.log('hello');
+console.log(addExpenses);
