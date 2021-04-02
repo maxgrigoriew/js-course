0dc09e047e71a7ef6c917aaa45c86341d44e253d
Max <sss123123111@yandex.ru>
2021-04-03 01:12:36 +0300

    lesson02

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let money = 50000;
+let income = 'web development';
+let addExpenses = 'продукты, такси, развдечения';
+let deposit = true;
+let mission = 100000;
+let period = 7;
+
+console.log(typeof income);
+console.log(typeof mission);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log(toString.)
