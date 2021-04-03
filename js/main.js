a28d06a0969d2cfe9d03eb8ff090d3588f788ac1
Max <sss123123111@yandex.ru>
2021-04-03 10:59:28 +0300

    add lesson02

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
-alert('привет');
+                            // lesson 02
+                            let money = 50000;
+let income = 'web development';
+let addExpenses = 'Продукты, Такси, Развлечения';
+let deposit = true;
+let mission = 100000;
+let period = 7;
+
+console.log(typeof income);
+console.log(typeof mission);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = 60000;
+budgetDay /= 30;
+console.log(budgetDay);
+
+let str = 'hello my frends!';
+
+console.log(str.length);
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str[0]);
+console.log(str.substring(1, 15));
+console.log(str.slice(-112));
+console.log(str.split(' '));
+console.log(addExpenses.split(', '));
