94b8efd15713241f04db3094a0ef6877467d5e86
Max <sss123123111@yandex.ru>
2021-04-03 10:53:40 +0300

    lesson02

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let money = 50000;
 let income = 'web development';
-let addExpenses = 'продукты, такси, развдечения';
+let addExpenses = 'Продукты, Такси, Развлечения';
 let deposit = true;
 let mission = 100000;
 let period = 7;
@@ -10,4 +10,27 @@ console.log(typeof mission);
 console.log(typeof deposit);
 console.log(addExpenses.length);
 console.log('Период равен ' + period + ' месяцев');
-console.log(toString.)
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = 60000;
+budgetDay /= 30;
+console.log(budgetDay);
+
+
+
+
+
+
+
+
+// let str = 'hello my frends!';
+
+// console.log(str.length);
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str[0]);
+// console.log(str.substring(1, 15));
+// console.log(str.slice(-112));
+// console.log(str.split(' '));
+// console.log(addExpenses.split(', '));
