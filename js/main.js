51668f0ea1b08dcf8f229e9de2adebec391afe24
Max <sss123123111@yandex.ru>
2021-04-14 22:50:27 +0300

    sadf

--- js/main.js
+++ js/main.js
@@ -1,37 +1,31 @@
 "use strict"
 
+
 let money = +prompt('Ваш месячный доход, руб', '15000');
 
-console.log(typeof money);
-// let addExpenses = prompt('Перечислите Вашы возможные расходы за расчитываемый ериод через запятую. Пример: Квартплата, проездной, кредит');
-let deposit = confirm('Есть ли у Вас депозит в банке?');
+let addExpenses = .split(prompt('Перечислите Вашы возможные расходы за расчитываемый ериод через запятую. Пример: Квартплата, проездной, кредит'));
 
-console.log(deposit);
+let deposit = confirm('Есть ли у Вас депозит в банке?');
 
 let expenses1 = prompt('Введите обязательную статью расходов');
 
-let expenses2 = prompt('Введите обязательную статью расходов');
-
 let amount1 = +prompt('Во сколько это обойдется?');
+
+let expenses2 = prompt('Введите обязательную статью расходов');
 
 let amount2 = +prompt('Во сколько это обойдется?');
 
-let budgetMonth = money - amount1;
-
-console.log("Ваш бюджет за месяц: " + budgetMonth + " " + " руб");
+let budgetMonth = money - amount1 -amount2;
 
 let mission = +prompt('Какую сумму составляеть ваша цель?', '555000');
 
 let months = Math.floor(mission / budgetMonth);
 
-console.log('Ваша цель будет достигнута через: ' + months + ' месяцев(-а)');
-
 let budgetDay = Math.floor(budgetMonth / 30);
 
-// console.log(budgetDay);
-console.log('Ваш бюджет на день составляет:' + budgetDay + ' руб.');
 
-if (budgetDay >= 12000) {
+
+if (budgetDay >= 1200) {
     console.log('У Вас высокий уровень дохода');
 }
 else if (budgetDay < 1200 && budgetDay > 600) {
@@ -43,3 +37,17 @@ else if (budgetDay < 600 && budgetDay >=0) {
 else {
     console.log('Что то пошло не так');
 }
+
+
+console.log(deposit);
+console.log(addExpenses);
+
+console.log("Ваш бюджет за месяц: " + budgetMonth + " " + " руб");
+
+console.log('Ваша цель будет достигнута через: ' + months + ' месяцев(-а)');
+
+console.log(typeof money);
+
+console.log(budgetDay);
+
+console.log('Ваш бюджет на день составляет:' + budgetDay + ' руб.');
