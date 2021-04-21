d1282e59e3468fd6da0b30b9eb5351d7592129a8
Max <sss123123111@yandex.ru>
2021-04-21 22:03:41 +0300

    fit let addExpenses

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
-
 'use strict';
 
-const isNumber = function (n){
+const isNumber = function (n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
 let money,
-    start = function() {
+    start = function () {
         do {
             money = prompt('Ваш месячный доход?');
         } while (!isNumber(money));
@@ -24,10 +23,10 @@ let appData = {
     budgetDay: 0,
     budgetMonth: 0,
     expensesMonth: 0,
-    
-    asking: function() {
-        let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-        appData.addExpenses = addExpenses.toLowerCase().split(', ');
+
+    asking: function () {
+        // let addExpenses = ;
+        appData.addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую').toLowerCase().split(', ');
         appData.deposit = confirm('Есть ли у Вас депозит в банке?');
 
         for (let i = 0; i < 2; i++) {
@@ -39,23 +38,23 @@ let appData = {
             appData.expenses[expenseName] = +expenseValue;
         }
     },
-    
-    getExpensesMonth: function() {
+
+    getExpensesMonth: function () {
         let sum = 0;
         for (const item in appData.expenses) {
             sum += appData.expenses[item];
         }
         appData.expensesMonth = sum;
     },
-    
-    getBudget: function() {
+
+    getBudget: function () {
         let budgetMonth = appData.budget - appData.expensesMonth;
         let budgetDay = Math.floor(budgetMonth / 30);
         appData.budgetMonth = budgetMonth;
         appData.budgetDay = budgetDay;
     },
-    
-    getTargetMonth: function() {
+
+    getTargetMonth: function () {
         if (appData.budgetMonth <= 0) {
             console.log('Цель не будет достигнута');
         } else {
@@ -63,13 +62,13 @@ let appData = {
             console.log('Цель будет достигнута за ' + targetMonth + ' месяцев');
         }
     },
-    
-    getStatusIncome: function() {
-        if ( appData.budgetDay > 1200 ) {
+
+    getStatusIncome: function () {
+        if (appData.budgetDay > 1200) {
             console.log('У Вас высокий уровень дохода');
-        } else if ( appData.budgetDay > 600 && appData.budgetDay <= 1200 ) {
+        } else if (appData.budgetDay > 600 && appData.budgetDay <= 1200) {
             console.log('У Вас средний уровень дохода');
-        } else if ( appData.budgetDay > 0 && appData.budgetDay <= 600 ) {
+        } else if (appData.budgetDay > 0 && appData.budgetDay <= 600) {
             console.log('У Вас низкий уровень дохода');
         } else {
             console.log('Что то пошло не так');
