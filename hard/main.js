e5618eb88142afed28dc1aeb57e8d7dde8cad3c4
Max <sss123123111@yandex.ru>
2021-04-22 16:18:57 +0300

    start task

---
+++ hard/main.js
@@ -0,0 +1,15 @@
+"use strict"
+
+const getRandomInt = function (min, max) {
+    
+}
+const getRandomInt = function (n) {
+    return !isNaN(parseInt(n) && isFinite(n))
+}
+
+cost gameBot = function (atЕempts, min, max) {
+    attempts = attempts || 10;
+    min = min || 0;
+    max = max || 100;
+    
+}
