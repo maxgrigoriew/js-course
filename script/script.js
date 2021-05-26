75552514dbf287f46b43bde0c2d14cd6104d07b5
Max <sss123123111@yandex.ru>
2021-05-26 06:48:00 +0300

    add timer

---
+++ script/script.js
@@ -0,0 +1,51 @@
+window.addEventListener('DOMContentLoaded', function () {
+  'use strict';
+
+  const addZero = (number) => {
+    if (number < 10) return ('0' + number);
+    else return number;
+  };
+
+  const timer = (deadline) => {
+    const timerSeconds = document.querySelector('#timer-seconds');
+    const timerMinutes = document.querySelector('#timer-minutes');
+    const timerHours = document.querySelector('#timer-hours');
+
+    const getTimerRemainder = () => {
+        const dateStop = new Date(deadline).getTime();
+        let dateNow  = new Date().getTime();
+    
+        let timerRemainder = (dateStop - dateNow) / 1000;
+        let seconds = Math.floor(timerRemainder % 60);
+        let minutes = Math.floor( (timerRemainder / 60) % 60 );
+        let hours = Math.floor((timerRemainder / 60 / 60) % 24);
+
+        return {timerRemainder, seconds, minutes, hours};
+    };
+
+    let idUpdateTimer = 0;
+
+    const updateTimer = () => {
+      let timerResult = getTimerRemainder();
+
+      timerSeconds.textContent = addZero(timerResult.seconds);
+      timerMinutes.textContent = addZero(timerResult.minutes);
+      timerHours.textContent   = addZero(timerResult.hours);
+
+      if (timerResult.timerRemainder < 0) {
+        clearInterval(idUpdateTimer);
+
+        timerSeconds.textContent = '00';
+        timerMinutes.textContent = '00';
+        timerHours.textContent = '00';
+      }
+    };
+
+    updateTimer();
+
+    idUpdateTimer = setInterval(updateTimer, 1000);
+  };
+
+  const deadline = new Date(' 27 may 2021');
+  timer(deadline);
+});
